2a18f1c4ddb726af15d72611cab3e50a03b0b7bd
klof44 <klof4four@gmail.com>
2023-06-09 23:23:30 -0500

    Website

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function CopyDiscord() {
+    navigator.clipboard.writeText('klof44#6612');
+
+    var popup = document.getElementById("copied-text");
+    popup.classList.remove("fade-out");
+    popup.classList.add("fade-in");
+    setTimeout(function(){ 
+        popup.classList.remove("fade-in"); 
+        popup.classList.add("fade-out")
+    }, 2000);
+}
